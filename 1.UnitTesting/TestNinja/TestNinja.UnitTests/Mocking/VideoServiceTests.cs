4df80b0eeb0569ed7070511ffa527aca633c719b
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-18 19:59:32 +0100

    Dependency Injection via Method Parameters & create VideoServiceTests with fake data (empty file)

---
+++ 1.UnitTesting/TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void readVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+           var result= service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result,Does.Contain("error").IgnoreCase);
+
+        }
+    }
+}
