6ab00baa3b0980cc97c3fe3884233758351029c8
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-18 17:50:26 +0100

    Testing Methods with string return type

---
+++ 1.UnitTesting/TestNinja/TestNinja.UnitTests/HTMLFormatterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HTMLFormatterTests
+    {
+
+
+        [Test]
+        public void FormatBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("abc");
+
+            //specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //general => Fault
+            // Assert.That(result, Does.StartWith("<strong>"));
+            // Assert.That(result, Does.EndtWith("</strong>"));
+            //Assert.That(result, Does.Contains("abc"));
+        }
+    }
+}
