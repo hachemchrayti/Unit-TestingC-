2146022b4aeb2af33b56bf3c48fa1a1f4ee84a1e
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-18 17:58:26 +0100

    Test methods with Array or Collection return type

--- 1.UnitTesting/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ 1.UnitTesting/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -75,5 +75,29 @@ namespace TestNinja.UnitTests
         //    var result = _math.Max(2, 2);
         //    Assert.That(result, Is.EqualTo(2));
         //}
+
+
+        [Test]
+        public void getOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result=_math.GetOddNumbers(5);
+
+            //general
+          //  Assert.That(result,Is.Not.Empty);
+            
+          //Assert.That(result.Count(),Is.EqualTo(3));
+            
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+
+            Assert.That(result,Is.EquivalentTo(new int[]{1,3,5 }));
+
+            //autres exemples d'assert
+            //Assert.That(result,Is.Ordered);
+            //Assert.That(result,Is.Unique);
+        }
+
+
     }
 }
